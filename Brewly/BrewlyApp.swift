dc3b0a92435be58f28aed55ee4806fef556f93f5
Justin Wang <droitinjuste@gmail.com>
2026-08-12 06:14:32 +0800

    Initial Commit

---
+++ Brewly/BrewlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrewlyApp.swift
+//  Brewly
+//
+//  Created by Justin Wang on 2026-08-12.
+//
+
+import SwiftUI
+
+@main
+struct BrewlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
